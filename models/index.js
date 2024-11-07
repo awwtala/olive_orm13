34d4e1edfe8331b2a87b0dbedb1983852a5fa056
Tala Awwad <awwadtala08@gmail.com>
2024-11-07 15:23:39 -0800

    adding products to index.js

--- models/index.js
+++ models/index.js
@@ -1,16 +1,32 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./models/Product");
+const Category = require("./models/Category");
+const Tag = require("./models/Tag");
+const ProductTag = require("./models/ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "categoryId", // This is the foreign key in the Product model
+  onDelete: "CASCADE", // This will delete products when the category is deleted
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "categoryId", // This is the foreign key in the Product model
+  onDelete: "CASCADE", // This will delete products when the category is deleted
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag, // This specifies the junction table
+  foreignKey: "productId", // Foreign key in the ProductTag table
+  otherKey: "tagId", // Foreign key in the ProductTag table
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag, // This specifies the junction table
+  foreignKey: "tagId", // Foreign key in the ProductTag table
+  otherKey: "productId", // Foreign key in the ProductTag table
+});
 
 module.exports = {
   Product,
