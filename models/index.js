c4fe94498cc7d3632ef67f7e9e2ed2ff1a66d3b1
Tala Awwad <awwadtala08@gmail.com>
2024-11-07 22:17:04 -0800

    fixes in index.js & adding routes to category

--- models/index.js
+++ models/index.js
@@ -6,26 +6,23 @@ const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: "categoryId", // This is the foreign key in the Product model
-  onDelete: "CASCADE", // This will delete products when the category is deleted
+  foreignKey: "category_id", // This is the foreign key in the Product model
+  //onDelete: "CASCADE", // This will delete products when the category is deleted
 });
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: "categoryId",
-  onDelete: "CASCADE",
+  foreignKey: "category_id",
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: "productId",
-  otherKey: "tagId", // Foreign key in the ProductTag table
+  foreignKey: "product_id",
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: "tagId",
-  otherKey: "productId",
+  foreignKey: "tag_id",
 });
 
 module.exports = {
